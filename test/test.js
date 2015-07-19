e5607a5eeefd97117901f5e57767d343f2e79e64
juner <juner@juner.net>
2015-07-19 17:28:59 +0900

    add simple use test.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,18 @@
 ;(function($,$Q,undefined){
     "use strict";
+	$Q.test("jQuery.texts simple use test",function(assert){
+		assert.ok("texts" in $.fn,"$(elm).texts is define.");
+	assert.ok($.isFunction($.fn["texts"]),"$().texts() is function.");
+		var $target = $("#target"),
+			$test,
+			$texts;
+		$test = $("<span>test</span>").appendTo($target);
+		$texts = $test.texts();
+		assert.equal($texts.length,4,"$(<span>test</span>).texts() -> get 4 elements.");
+		$test = $("<span>t e s t</span>").appendTo($target);
+		$texts = $test.texts();
+		assert.equal($texts.length,4,"$(<span>t e s t </span>).texts() -> get 4 elements.");
+	});
 	$Q.test("jQuery.texts API define check.",7,function(assert){
 		assert.ok("texts" in $,"jQuery.texts is define.");
 		assert.ok($.isFunction($.fn["texts"]),
