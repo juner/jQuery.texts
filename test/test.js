b6eca677427506abddbbb399ad1e811c2a8fff29
j.maeda <j.maeda@activeltd.co.jp>
2017-02-21 11:21:20 +0900

    テストを見やすくする為に構造を整理

--- test/test.js
+++ test/test.js
@@ -1,41 +1,35 @@
 ;(function($,$Q,undefined){
     "use strict";
-	$Q.test("jQuery.texts simple use test",function(assert){
-		assert.ok("texts" in $.fn,"$(elm).texts is define.");
-	assert.ok($.isFunction($.fn["texts"]),"$().texts() is function.");
-		var $target = $("#target"),
-			$test,
-			$texts;
-		$test = $("<span>test</span>").appendTo($target);
-		$texts = $test.texts();
-		assert.equal($texts.length,4,"$(<span>test</span>).texts() -> get 4 elements.");
-		$test = $("<span>t e s t</span>").appendTo($target);
-		$texts = $test.texts();
-		assert.equal($texts.length,4,"$(<span>t e s t </span>).texts() -> get 4 elements.");
+	$Q.module("API define check.",function(assert){
+		$Q.test("$(elm).texts is function.",function(assert){
+			assert.ok($.isFunction($.fn["texts"]))
+		});
+		$Q.test("jQuery.texts is define.",function(assert){
+			assert.ok("texts" in $);
+		});
+		[
+			"getOptions",
+			"setOptions",
+			"getTextsTargetTextNode",
+			"convertTextsWrapTextNode",
+			"getTexts"
+		].forEach(function(value,index){
+			$Q.test("jQuery.texts." + value + " is function.",function(assert){
+				assert.ok(value in $.texts);
+				assert.ok($.isFunction($.texts[value]));
+			})
+		});
 	});
-	$Q.test("jQuery.texts API define check.",7,function(assert){
-		assert.ok("texts" in $,"jQuery.texts is define.");
-		assert.ok($.isFunction($.fn["texts"]),
-				  "jQuery.fn.texts is de fine.");
-		assert.ok($.isFunction($.texts.getOptions),
-				  "jQuery.texts.getOptions is define.");
-		assert.ok($.isFunction($.texts.setOptions),
-				  "jQuery.texts.setOptions is define.");
-		assert.ok($.isFunction($.texts.getTextsTargetTextNode),
-				 "jQuery.texts.getTextsTargetTextNode is define.");
-		assert.ok($.isFunction($.texts.convertTextsWrapTextNode),
-				 "jQuery.texts.convertTextsWrapTextNode is define.");
-
-		assert.ok($.isFunction($.texts.getTexts),
-				  "jQuery.texts.getTexts is define.");
-	});
-	$Q.test("jQuery.texts",function(assert){
-		var $target = $("#target");
-		assert.ok($.isFunction($target.texts),"$.fn.texts is define.");
-		var message = "text";
-		$target.text(message);
-		var $texts = $target.texts();
-		assert.equal($texts.length,message.length,"$elm.texts() でちゃんと小分けになって取得できているか");
-        assert.deepEqual($texts.end().get(),$target.get(),".texts().end() is prev element.");
+	$Q.module("simple use", function(hooks){
+		$Q.test("$(<span>test</span>).texts() -> get 4 elements.",function(assert){
+			var $test = $("<span>test</span>").appendTo($("#target"));
+			var $texts = $test.texts();
+			assert.equal($texts.length,4,"$(<span>test</span>).texts() -> get 4 elements.");
+		});
+		$Q.test("$(<span>t e s t </span>).texts() -> get 4 elements.",function(assert){
+			var $test = $("<span>t e s t</span>").appendTo($("#target"));
+			var $texts = $test.texts();
+			assert.equal($texts.length,4,"$(<span>t e s t </span>).texts() -> get 4 elements.");
+		});
 	});
 })(jQuery,QUnit);
