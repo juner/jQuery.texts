8d5d73c2c88445defd36b3bd0f96d1cc7c27f178
j.maeda <j.maeda@activeltd.co.jp>
2017-02-22 14:23:06 +0900

    文字の分割チェックに関するテストの整理

--- test/test.js
+++ test/test.js
@@ -22,38 +22,26 @@
 	});
 	$Q.module("simple use", function(hooks){
 		var $span = $.parseHTML("<span></span>");
-		$Q.test("test -> get 4 elements.",function(assert){
-			var $test = $($span).clone().text("test").appendTo($("#target"));
-			var $texts = $test.texts();
-			assert.equal($texts.length,4);
-		});
+		var test = function(context,answer){
+			$Q.test(context+ " -> get "+answer.length+" elements.",function(assert){
+				var $test = $($span).clone().text(context).appendTo($("#target"));
+				var $texts = $test.texts();
+				var result = $texts.map(function(index,$element){ return $($element).text(); }).get();
+				assert.deepEqual(answer,result);
+			});
+		};
+		test("test",["t","e","s","t"]);
 		$Q.module("exclude space charactor.",function(hooks){
-			$Q.test("t e s\tt -> get 4 elements.",function(assert){
-				var $test = $($span).clone().text("t e s\tt ").appendTo($("#target"));
-				var $texts = $test.texts();
-				assert.equal($texts.length,4);
-			});
+			test("t e s\tt ",["t","e","s","t"]);
 		});
-		$Q.module("surrogate pair support.",function(assert){
-			$Q.test("\uD867\uDE3D -> get 1 elements",function(assert){
-				var $test = $($span).clone().text("\uD867\uDE3D").appendTo($("#target"));
-				var $texts = $test.texts();
-				assert.equal($texts.length,1);
-			});
+		$Q.module("surrogate pair support.",function(hooks){
+			test("\uD867\uDE3D\uD842\uDFB7",["\uD867\uDE3D","\uD842\uDFB7"]);
 		});
-		$Q.module("regature charactor unsupport.",function(assert){
-			$Q.test("A\u20E0秘\u20E3 -> get 4 elements",function(assert){
-				var $test = $($span).clone().text("A\u20E0秘\u20E3").appendTo($("#target"));
-				var $texts = $test.texts();
-				assert.equal($texts.length,4);
-			});
+		$Q.module("regature charactor unsupport.",function(hooks){
+			test("A\u20E0秘\u20E3",["A","\u20E0","秘","\u20E3"]);
 		});
-		$Q.module("variation selector support.",function(){
-			$Q.test("\u2668\ufe0e\u24c2\ufe0f\u908A\uDB40\uDD00 -> get 3 elements",function(assert){
-				var $test = $($span).clone().text("\u2668\ufe0e\u24c2\ufe0f\u908A\uDB40\uDD00").appendTo($("#target"));
-				var $texts = $test.texts();
-				assert.equal($texts.length,3);
-			});
+		$Q.module("variation selector support.",function(hooks){
+			test("\u2668\ufe0e\u24c2\ufe0f\u908A\uDB40\uDD00\t",["\u2668\ufe0e","\u24c2\ufe0f","\u908A\uDB40\uDD00"]);
 		});
 	});
 })(jQuery,QUnit);
